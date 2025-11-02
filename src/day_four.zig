5acd2305db277c6e32183076de9b076b45201794
Gary Moore <gary@garypotatoes.xyz>
2025-11-02 15:08:39 +0000

    scaffold out day four

---
+++ src/day_four.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+pub fn p1() void {
+    std.debug.print("Not implemented\n", .{});
+}
--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,4 @@
 pub const day_one = @import("./day_one.zig");
 pub const day_two = @import("./day_two.zig");
 pub const day_three = @import("./day_three.zig");
+pub const day_four = @import("./day_four.zig");
